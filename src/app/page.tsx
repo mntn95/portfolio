97f54fca9e6a084dc12db076d3218a59a94ed9fa
Mathieu NG <mattngtn@gmail.com>
2025-01-16 17:55:55 +0100

    ✨ Add skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 
-import { About, Hero, Experiences } from "@/components";
+import { About, Hero, Experiences, Skills } from "@/components";
 
 const Home = () => (
     <>
         <Hero />
         <About />
         <Experiences />
+        <Skills />
     </>
 );
 
---
+++ src/components/skills/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import * as React from "react";
+
+import { skillsData } from "@/assets";
+
+import Heading from "../base-components/heading";
+import Skill from "./skill";
+
+const Skills = (): React.ReactNode => (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-y-20 px-96">
+        <Heading text="Skills" />
+        <div className="w-full flex justify-between flex-wrap gap-x-8 gap-y-10 lg:gap-y-6">
+            {skillsData.map(({ icon, name }, index) => (
+                <Skill key={index} icon={icon} name={name} index={index} />
+            ))}
+        </div>
+    </div>
+);
+
+export default Skills;
